966c8a307d14cf1014ec7dcb458a5b4f91b5f2af
morganlutz <morgan.lutz@gmail.com>
2016-09-03 16:51:23 -0700

    Implement slack command

---
+++ app.js
@@ -0,0 +1,96 @@
+const Botkit = require('botkit');
+const fs = require('fs');
+const logger = require('morgan');
+
+const RobinBot = {
+  exclamationsByLetter: {}
+};
+
+try {
+  RobinBot.tokens = JSON.parse(fs.readFileSync(__dirname + '/private/auth.json'));
+  RobinBot.exclamations = JSON.parse(fs.readFileSync(__dirname + '/data/exclamations.json'));
+} catch(error) {
+  RobinBot.authToken = '';
+  RobinBot.exclamations = ['Failure'];
+  console.log('Missing valid exclamations file or auth token.');
+  console.error(error);
+}
+
+const logFormat = ':remote-addr - :remote-user [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :response-time ms';
+
+const controller = Botkit.slackbot({
+  debug: true,
+  json_file_store: './data/storage.json'
+});
+
+const bot = controller.spawn({
+  token: RobinBot.tokens.authToken
+});
+
+bot.startRTM((err) => {
+  if (err) {
+    console.error('Error occurred while starting bot.', err);
+  }
+});
+
+bot.api.team.info({}, (err, res) => {
+  if(err) {
+    return console.error(err);
+  }
+
+  controller.saveTeam(res.team, (err) => {
+    if(err) {
+      console.error(err);
+    }
+  });
+});
+
+for(let [, exclamation] of RobinBot.exclamations.entries()) {
+
+  if(!RobinBot.exclamationsByLetter.hasOwnProperty(exclamation[0])) {
+    RobinBot.exclamationsByLetter[exclamation[0]] = [];
+  }
+
+  RobinBot.exclamationsByLetter[exclamation[0]].push(exclamation);
+}
+
+controller.setupWebserver(3000, (err, webserver) => {
+  webserver.use(logger(logFormat, {
+    stream: require('file-stream-rotator').getStream({
+      date_format: 'YYYY-MM-DD',
+      filename: `${__dirname}/logs/development-%DATE%.log`,
+      frequency: '7d',
+      verbose: false
+    })
+  }));
+
+  controller.createWebhookEndpoints(webserver);
+});
+
+controller.on('tick', () => true);
+
+controller.on('slash_command', (bot, message) => {
+  console.log('onSlashCommand!', message);
+
+  if (message.token !== RobinBot.tokens.commandVerifyToken) {
+    return bot.res.send(401, 'Unauthorized');
+  }
+
+  bot.res.send(200, '');
+
+  if(RobinBot.exclamationsByLetter.hasOwnProperty(message.text)) {
+    let exclamations = RobinBot.exclamationsByLetter[message.text];
+
+    let reply = 'Holy ' + exclamations[Math.floor(Math.random() * exclamations.length)] + '!';
+    bot.replyPublicDelayed(message, reply);
+
+  } else if(message.text.length === 0) {
+    let reply = 'Holy ' + RobinBot.exclamations[Math.floor(Math.random() * RobinBot.exclamations.length)] + '!';
+    bot.replyPublicDelayed(message, reply);
+
+  } else {
+    bot.replyPrivateDelayed(message, 'Holy Try Again! Send an empty message or A-Z!');
+
+  }
+
+});
